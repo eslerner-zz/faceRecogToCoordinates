9a2eca0ed06b2abdb51fcc938a1e969872ea05d1
eslerner <eslerner@umich.edu>
2017-06-26 21:50:08 -0400

    Add files via upload

--- svgToCoord.py
+++ svgToCoord.py
@@ -1,70 +1,47 @@
-import numpy
-import svg.path
-import xml.dom
-import svgpathtools
-import math
 from xml.dom import minidom
 from svg.path import parse_path
 
+
+class DynamicList(list):
+    def __getslice__(self, i, j):
+        return self.__getitem__(slice(i, j))
+
+    def __setslice__(self, i, j, seq):
+        return self.__setitem__(slice(i, j), seq)
+
+    def __delslice__(self, i, j):
+        return self.__delitem__(slice(i, j))
+
+    def _resize(self, index):
+        n = len(self)
+        if isinstance(index, slice):
+            m = max(abs(index.start), abs(index.stop))
+        else:
+            m = index + 1
+        if m > n:
+            self.extend([self.__class__() for i in range(m - n)])
+
+    def __getitem__(self, index):
+        self._resize(index)
+        return list.__getitem__(self, index)
+
+    def __setitem__(self, index, item):
+        self._resize(index)
+        if isinstance(item, list):
+            item = self.__class__(item)
+        list.__setitem__(self, index, item)
+
+
 svg_dom = minidom.parse("C:\Users\Emily\Downloads\Apple\homer-simpson.svg")
 path_strings = [path.getAttribute('d') for path in svg_dom.getElementsByTagName('path')]
+i = 0
 z = 0
-
+LinePos = DynamicList()
 for path_string in path_strings:
     path_data = parse_path(path_string)
-    w, h = int(math.floor(path_data.length())), 2;
-
-    LinePos = [[0 for x in range(w)] for y in range(h)]
-    row = 0
-    columns = 0
-    for point in path_data:
-        for point2 in range(0,1,int(1/.01)):
-            x =  int(path_data.point(point2).real)
-            y = int(path_data.point(point2).imag)
-            print(x)
-            print(y)
-
-#grab the svg file
-#xmlFile = minidom.parse("C:\Users\Emily\Downloads\Apple\homer-simpson.svg")
-
-#path_strings = [path.getAttribute('d') for path in xmlFile.getElementsByTagName('path')]
-#for path_string in path_strings:#for every string,
- #   path_data = parse_path(path_string) #return the data for that string (ex: start, controls, end) for eaach piece of info
-  #  print(path_data)
-   # for point in range(0,1):
-    #    print(path_data.)
-
-
-
-
-
-  #  length = path_data.length()
- #   for i in range(1,int(math.floor(length - 1))):
-        #point = path_data.point(i)
-       # print(point)
-    #x = pointAtZero.x
-    #print(length)
-    #print(pointAtZero)
-
-
-
-    #for i in range(path_data., int(math.floor(length))):
-     #   path12 = path_data.point(i)
-    #    print(path12)
-    #print(length)
-    #print(path_data)
-
-#inside svg tags
-#svgElems = xmlFile.getElementsByTagName("svg")[0]
-
-#inside "g" tags
-#gElems = svgElems.getElementsByTagName("g")[0]
-
-#find the paths
-#paths = gElems.getElementsByTagName("path")
-
-#for every path line, get that instruction.
-#for p in paths:
-   # a = p.getAttribute('class')
- #   svgInstructions = p.getAttribute('d')
-   # print(svgInstructions)
+    for point in range(0, 1, int(1 / .001)):
+        x = int(path_data.point(point).real)
+        y = int(path_data.point(point).imag)
+        LinePos[i] = [x, y, z]
+        print(LinePos[i])
+i = (i + 1)
